4ed7f28956bb0c3ec382f94ba9b7ea1ce0a54657
jsd1982 <james.jdunne@gmail.com>
2024-01-26 17:26:01 -0600

    Properly sending multipart data from frontend to backend and storing in redis

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"crypto/sha512"
+	"encoding/base64"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"io"
 	"log"
 	"mime"
-	"net/http"
+	"mime/multipart"
 	"os"
-
-	"github.com/gin-gonic/gin"
+	"runtime"
+	"strings"
 )
+
+var rcl *redis.Client
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -27,28 +34,130 @@ func CORSMiddleware() gin.HandlerFunc {
 }
 
 func main() {
+	ConfigRuntime()
+	ConnectRedis()
+	StartGin()
+}
+
+// ConfigRuntime sets the number of operating system threads.
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	fmt.Printf("Running with %d CPUs\n", nuCPU)
+}
+
+func ConnectRedis() {
 	var err error
+
+	var opts *redis.Options
+	opts, err = redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		log.Fatalf("error parsing $REDIS_URL: %v\n", err)
+	}
+	log.Printf("redis: %+v\n", opts)
+
+	rcl = redis.NewClient(opts)
+}
+
+// StartGin starts gin web server with setting router.
+func StartGin() {
+	var err error
+
+	// default to production mode if no GIN_MODE set:
+	if os.Getenv(gin.EnvGinMode) == "" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	err = mime.AddExtensionType(".js", "application/javascript")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	//http.Handle("/", http.FileServer(http.Dir("public")))
-	//http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("dist"))))
-
-	router := gin.Default()
+	router := gin.New()
 	router.SetTrustedProxies(nil)
 	router.Use(CORSMiddleware())
-	router.NoRoute(gin.WrapH(http.FileServer(gin.Dir("public", false))))
+
+	router.StaticFile("/", "public/index.html")
+	router.StaticFile("/index.html", "public/index.html")
 	router.Static("/js/", "dist")
-	router.POST("/save", func(ctx *gin.Context) {
-		b, err := io.ReadAll(ctx.Request.Body)
+
+	// TODO: CSRF to prevent anyone from posting to this endpoint
+	router.POST("/save", func(c *gin.Context) {
+		var err error
+
+		var mr *multipart.Reader
+		mr, err = c.Request.MultipartReader()
 		if err != nil {
-			log.Printf("")
+			log.Printf("error getting MultipartReader: %v\n", err)
+			c.AbortWithError(400, err)
+			return
 		}
-		ctx.Writer.WriteHeader(200)
-		ctx.Writer.Write(b)
-		ctx.Writer.Flush()
+
+		var header, wram, sram []byte = nil, nil, nil
+
+		// read parts:
+		for {
+			var part *multipart.Part
+			part, err = mr.NextPart()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("error advancing MultipartReader::NextPart: %v\n", err)
+				c.AbortWithError(500, err)
+				return
+			}
+
+			var b *[]byte
+			switch strings.ToLower(part.FileName()) {
+			case "header":
+				b = &header
+				break
+			case "wram":
+				b = &wram
+				break
+			case "sram":
+				b = &sram
+				break
+			}
+
+			*b, err = io.ReadAll(part)
+			if err != nil {
+				log.Printf("error reading from multipart.Part: %v\n", err)
+				c.AbortWithError(500, err)
+				return
+			}
+		}
+
+		// hash all the contents together:
+		h := sha512.New()
+		h.Write(header)
+		h.Write(wram)
+		if sram != nil {
+			h.Write(sram)
+		}
+
+		// url-base64 encode that hash (with no padding = chars):
+		hs := base64.RawURLEncoding.EncodeToString(h.Sum(nil))
+
+		// redis HSET:
+		values := make([]any, 0, 6)
+		values = append(values, "header", header)
+		values = append(values, "wram", wram)
+		if sram != nil {
+			values = append(values, "sram", sram)
+		}
+		err = rcl.HSet(c, hs, values...).Err()
+		if err != nil {
+			log.Printf("error redis HSET: %v\n", err)
+			c.AbortWithError(500, err)
+			return
+		}
+
+		c.Header("Content-Type", "text/html")
+		c.Writer.WriteHeaderNow()
+		link := fmt.Sprintf(`<a href="/d/%s"></a>`, hs)
+		c.Writer.WriteString(link)
 	})
 
 	port := os.Getenv("PORT")
--- ts/index.ts
+++ ts/index.ts
@@ -4,6 +4,7 @@ import {GrpcWebFetchTransport} from "@protobuf-ts/grpcweb-transport";
 
 import {
     AddressSpace,
+    DetectMemoryMappingRequest,
     DevicesRequest,
     DevicesResponse,
     MemoryMapping,
@@ -20,14 +21,44 @@ function setupTransport(baseUrl: string = 'http://localhost:8190') {
 const devicesClient = new DevicesClient(setupTransport());
 const memoryClient = new DeviceMemoryClient(setupTransport());
 
+class Capture {
+    header: Uint8Array; // ROM header from FFB0..FFFF
+    wram: Uint8Array;
+    sram: Uint8Array | null;
+
+    constructor() {
+        this.header = new Uint8Array(0x10000 - 0xFFB0);
+        this.wram = new Uint8Array( 0x20000);
+        this.sram = null;
+    }
+
+    sramSize() {
+        let sramSize = this.header[0xFFD8 - 0xFFB0];
+        if (sramSize > 8) {
+            // unlikely a valid value:
+            return 0;
+        }
+        // amount of SRAM data in bytes:
+        return 1024 << sramSize;
+    }
+
+    reset() {
+        this.header.fill(0);
+        this.wram.fill(0);
+        this.sram = null;
+    }
+}
+
 // list all devices:
 document.addEventListener('DOMContentLoaded', function () {
     let uri = '';
+    let capture = new Capture();
+
     let lstDevices = document.getElementById('lstDevices');
     let btnCapture = document.getElementById('btnCapture');
 
     // enable/disable Capture button depending on selected device:
-    lstDevices.addEventListener('change', function (this: HTMLSelectElement, e:Event) {
+    lstDevices.addEventListener('change', function (this: HTMLSelectElement) {
         if (this.selectedIndex != 0) {
             uri = this.selectedOptions[0].value;
             btnCapture.removeAttribute('disabled');
@@ -38,13 +69,52 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // click Capture button:
-    btnCapture.addEventListener('click', function () {
+    btnCapture.addEventListener('click', async function () {
         if (uri === '') return;
+
+        capture.reset();
+
+        // let SNI detect the memory mapping:
+        let drsp;
+        try {
+            drsp = await memoryClient.mappingDetect(DetectMemoryMappingRequest.create({
+                uri: uri,
+                fallbackMemoryMapping: MemoryMapping.LoROM,
+            }));
+        } catch (err) {
+            console.warn("detect failed:", err);
+            return;
+        }
+
+        // read the ROM header:
+        console.log("read ROM header:");
+        let rsp;
+        try {
+            rsp = await memoryClient.singleRead(SingleReadMemoryRequest.create({
+                uri: uri,
+                request: ReadMemoryRequest.create({
+                    requestAddressSpace: AddressSpace.SnesABus,
+                    requestMemoryMapping: drsp.response.memoryMapping,
+                    requestAddress: 0x00FFB0,
+                    size: 0x50
+                })
+            }));
+        } catch (err) {
+            console.warn("read failed:", err);
+            return;
+        }
+
+        console.log("read ROM header complete:");
+        let mrsp = rsp.response.response;
+        if (!mrsp) return;
+
+        console.log(mrsp.data);
+        capture.header.set(mrsp.data);
 
         // read all of WRAM:
         console.log("read WRAM");
         try {
-            memoryClient.singleRead(SingleReadMemoryRequest.create({
+            rsp = await memoryClient.singleRead(SingleReadMemoryRequest.create({
                 uri: uri,
                 request: ReadMemoryRequest.create({
                     requestAddressSpace: AddressSpace.FxPakPro,
@@ -52,20 +122,66 @@ document.addEventListener('DOMContentLoaded', function () {
                     requestAddress: 0xF50000,
                     size: 0x020000
                 })
-            })).then(
-                rsp => {
-                    console.log("read WRAM complete:");
-                    const mrsp = rsp.response.response;
-                    console.log(mrsp?.data);
-                },
-                err => {
-                    console.log("read WRAM failed:", err);
-                }
-            ).catch(reason => {
-                console.log("read WRAM catch: ", reason);
+            }));
+        } catch (err) {
+            console.warn("read failed:", err);
+            return;
+        }
+
+        console.log("read WRAM complete:");
+        mrsp = rsp.response.response;
+        if (!mrsp) return;
+
+        console.log(mrsp.data);
+        capture.wram.set(mrsp.data);
+
+        const sramSize = capture.sramSize();
+        if (sramSize > 0) {
+            console.log("read SRAM " + sramSize + " bytes");
+            try {
+                rsp = await memoryClient.singleRead(SingleReadMemoryRequest.create({
+                    uri: uri,
+                    request: ReadMemoryRequest.create({
+                        requestAddressSpace: AddressSpace.FxPakPro,
+                        requestMemoryMapping: MemoryMapping.Unknown,
+                        requestAddress: 0xE00000,
+                        size: sramSize
+                    })
+                }));
+            } catch (err) {
+                console.warn("read failed:", err);
+                return;
+            }
+
+            console.log("read SRAM complete:");
+            mrsp = rsp.response.response;
+            if (!mrsp) return;
+
+            console.log(mrsp.data);
+            capture.sram = mrsp.data;
+        }
+
+        // ship off to /save endpoint:
+        let formData = new FormData();
+        formData.append("section", new Blob([capture.header]), "header");
+        formData.append("section", new Blob([capture.wram]), "wram");
+        if (capture.sram) {
+            formData.append("section", new Blob([capture.sram]), "sram");
+        }
+
+        let wrsp;
+        try {
+            wrsp = await fetch("/save", {
+                method: "POST",
+                body: formData
             });
-        } catch (ex) {
-            console.log("catch: ", ex);
+        } catch (err) {
+            console.warn("error POSTing to /save: ", err);
+            return;
+        }
+
+        if (wrsp.status >= 300) {
+
         }
     });
 
@@ -74,7 +190,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let lastDeviceUris: string[] = ['garbage'];
         const onfulfilled = (devices: FinishedUnaryCall<DevicesRequest, DevicesResponse>) => {
             let devs = devices.response.devices;
-            console.log(JSON.stringify(devs));
+            // console.log(JSON.stringify(devs));
 
             let nextDeviceUris: string[] = [];
             for (let i = 0; i < devs.length; i++) {
@@ -94,18 +210,19 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
             if (diff) {
+                console.log(JSON.stringify(devs));
                 // update the dropdown list:
                 let opts: Node[] = [];
                 {
                     const el = document.createElement('option');
-                    el.text = `Select a SNES Device`;
+                    el.text = `-- Select a SNES Device --`;
                     el.value = '';
                     opts.push(el);
                 }
                 for (let i = 0; i < devs.length; i++) {
                     const d = devs[i];
                     const el = document.createElement('option');
-                    el.text = `${d.kind} ${d.displayName}`;
+                    el.text = `${d.kind}: ${d.displayName}`;
                     el.value = d.uri;
                     opts.push(el);
                 }
@@ -114,6 +231,14 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         };
         const onrejected = (reason: any) => {
+            let opts: Node[] = [];
+            {
+                const el = document.createElement('option');
+                el.text = `SNI is required to use this tool`;
+                el.value = '';
+                opts.push(el);
+            }
+            lstDevices.replaceChildren(...opts);
             console.log(reason);
         };
 
@@ -123,7 +248,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 return devicesClient.listDevices(DevicesRequest.create())
                     .then(onfulfilled, onrejected);
             },
-            750
+            2000
         );
         setTimeout(
             () => {
